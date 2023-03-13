1eda02677c809eaf45f51227a6cbde8dd10b4d3f
sanychiquetti <sanymara77@hotmail.com>
2023-03-13 15:06:02 -0300

    exercício 35

--- exercicios/exe035.py
+++ exercicios/exe035.py
@@ -2,3 +2,10 @@
 Desenvolva um programa que leia o comprimento de três retas e diga ao usuário
 se elas podem ou não formar um triângulo.
 """
+ladoA = input('Digite o primeiro lado: ')
+ladoB = input('Digite o segundo lado: ')
+ladoC = input('Digite o terceiro lado: ')
+if (ladoA + ladoB <= ladoC) or (ladoB + ladoC <= ladoA):
+    print('Não é um triângulo!')
+else:
+    print('É um triângulo!')
