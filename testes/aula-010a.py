cb25031233781baf2165d266db086c25776c54e0
sanychiquetti <sanymara77@hotmail.com>
2023-03-17 14:44:49 -0300

    refatorando os códigos

--- testes/aula-010a.py
+++ testes/aula-010a.py
@@ -4,8 +4,8 @@ if tempo <= 3:
     print('Seu carro é novo')
 else:
     print('Seu carro é velho')
-print('--FIM--')
-"""
+print('--FIM--')"""
+
 # outra forma de fazer mais simplificada:
 tempo = int(input('Quantos anos tem seu carro? '))
 print('Seu carro é novo' if tempo<=3 else 'Seu carro é velho')
